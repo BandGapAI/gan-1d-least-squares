20fe148eceb7ad89418a10a6a557dd798ac98332
Kirill Kondrashov <kkondrashov@gnhearing.com>
2021-12-03 12:38:37 +0100

    refactor: move some math functions to `math_utils.py`

---
+++ math_utils.py
@@ -0,0 +1,8 @@
+import math
+
+def Phi(x):
+  return math.erf(x)
+def Phi_plus(x):
+  return 1 + Phi(x)
+def Phi_minus(x):
+  return 1 - Phi(x)
--- utils.py
+++ utils.py
@@ -2,6 +2,34 @@ import os
 from scipy.io import savemat, loadmat
 import matplotlib.pyplot as plt
 
+def show_paths(b_paths, h_paths):
+  """
+  Show path in b/h parameter space
+  """
+  b_paths, h_paths = np.array(b_paths), np.array(h_paths)
+
+  fig = plt.figure(figsize=(10, 10))
+
+  mean_b = np.mean(b_paths, axis=0)
+  std_b = np.std(b_paths, axis=0)
+  mean_h = np.mean(h_paths, axis=0)
+  std_h = np.std(h_paths, axis=0)
+  plt.plot(mean_b, label=r'$\bar b \pm 3\sigma$')
+  plt.fill_between(range(len(mean_b)), mean_b - 3*std_b, mean_b + 3*std_b, alpha=0.7)
+  plt.plot(mean_h, label=r'$\bar h \pm 3\sigma$')
+  plt.fill_between(range(len(mean_h)), mean_h - 3*std_h, mean_h + 3*std_h, alpha=0.7)
+  plt.legend()
+  plt.show()
+  print('solution b = %1.4f +/- %1.4f, h = %1.4f +/- %1.4f' %
+        (mean_b[-1], 3*std_b[-1], mean_h[-1], 3*std_h[-1]))
+
+def create_3d_plot():
+  fig = plt.figure(figsize=(12, 10))
+  ax = fig.gca(projection='3d')
+  ax.view_init(azim=45, elev=20)
+  ax.invert_xaxis()
+  return fig, ax
+
 def load_analytical(fname, param):
   """
   Utility functions to compare with analytical implementation
