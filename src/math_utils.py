4d1c1109a36b04c45be8176ee04444663f959f89
Kirill Kondrashov <kkondrashov@gnhearing.com>
2023-06-01 12:58:29 +0200

    fix: formatting

--- src/math_utils.py
+++ src/math_utils.py
@@ -1,8 +1,13 @@
 import math
 
+
 def Phi(x):
-  return math.erf(x)
+    return math.erf(x)
+
+
 def Phi_plus(x):
-  return 1 + Phi(x)
+    return 1 + Phi(x)
+
+
 def Phi_minus(x):
-  return 1 - Phi(x)
+    return 1 - Phi(x)
