bbd9c75685b16faf282958abca4ba73fa4a5e8f1
Kirill Kondrashov <kkondrashov@gnhearing.com>
2023-10-20 15:18:50 +0200

    WIP

--- content/pdf2eps.sh
+++ content/pdf2eps.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-files=( cost gh_test_case_c2_1_g0_9_h1_5.pdf )
-
-for f in "${files[@]}"; do
+for f in *.pdf; do
   echo transforming $f
-  pdftops -eps -r 600 $f.pdf $f.eps
+  pdftops -eps -r 600 $f $(basename -s .pdf $f).eps
 done
